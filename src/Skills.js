f0111fcaaeafe39e02afa823126adb3a87520d6b
EnTing0417 <en.ting88@gmail.com>
2020-02-20 18:21:22 +0800

    Your awesome message

---
+++ src/Skills.js
@@ -0,0 +1,41 @@
+// src/Skills.js
+ 
+import React from "react"
+ 
+function Skills(props) {
+  return (
+    <div>
+      <h2>Skills</h2>
+
+     <b><p>Tools and Technologies</p></b>
+       <ul>
+       	<li>Java</li>
+        <li>Bootstrap</li>
+        <li>Python</li>
+        <li>Deep Learning</li>
+		<li>Javascript</li>
+		<li>PHP</li>
+		<li>Image Processing</li>
+		<li>HTML & CSS</li>
+		<li>Visual Basic</li>
+		<li>C</li>
+		<li>MySQL</li>
+		<li>Adobe Premiere Pro</li>
+		<li>Adobe Photoshop</li>
+		<li>Chatbot Development</li>
+		<li>Robotic Programming</li>
+	 </ul>
+
+      <b><p>Industry Knowledge</p></b>
+       <ul>
+       	<li>Web Development</li>
+        <li>Machine Learning</li>
+          <li>Data Mining</li>
+		<li>Social Media Marketing</li>
+	 </ul>
+
+    </div>
+  )
+}
+ 
+export default Skills;
---
+++ src/Working_Experience.js
@@ -0,0 +1,42 @@
+// src/Working_Experience.js
+ 
+import React from "react"
+ 
+function Working_Experience(props) {
+  return (
+    <div>
+      <h2>Working Experience</h2>
+
+      <b><p>Junior Implementation Engineer - Internship @ Ematic Solutions Sdn Bhd</p></b>
+       <ul>
+		<li>Aug 2019 - Jan 2020</li>
+	 </ul>
+
+
+	  <b><p>Primary School Tutor @ Happy Learning Centre</p></b>
+       <ul>
+		<li>Jul 2018 - Aug 2018</li>
+	 </ul>
+
+	  <b><p>Social Media Marketer @ Revsolution Sdn.Bhd</p></b>
+       <ul>
+		<li>Apr 2016 - Aug 2017</li>
+	 </ul>
+
+
+	  <b><p>Social Media Marketer @ Foooqy Sdn.Bhd</p></b>
+       <ul>
+		<li>Apr 2016 - Aug 2017</li>
+	 </ul>
+
+
+	  <b><p>Social Media Marketer @ Mose Trade Sdn.Bhd</p></b>
+       <ul>
+		<li>Dec 2015 - Apr 2016</li>
+	 </ul>
+
+    </div>
+  )
+}
+ 
+export default Working_Experience;
